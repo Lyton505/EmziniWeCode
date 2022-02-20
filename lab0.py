5ec54b48f169e27fb772a5ba3e5021e06058e426
Lyton <65817176+Lyton505@users.noreply.github.com>
2022-02-20 17:09:46 +0200

    Add files via upload

---
+++ lab0.py
@@ -0,0 +1,14 @@
+# Replace with your email. Surround it by double quotes
+email = "ltmhlanga505@gmail.com"
+
+
+def twenty_twenty_two():
+    """Come up with the most creative expression that evaluates to 2022,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_two()
+    2022
+    """
+    return ((1000 * 3) + 40) - (18 + (100 * 10))
+
+
